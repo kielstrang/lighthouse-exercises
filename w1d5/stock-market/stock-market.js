3789ea8ed4a18a615b006dd765c14af5a9905fc5
vagrant <kiel.strang@gmail.com>
2017-09-07 22:11:08 +0000

    stock-market.js now returns -1 if no profit possible

--- w1d5/stock-market/stock-market.js
+++ w1d5/stock-market/stock-market.js
@@ -1,10 +1,12 @@
 function maxProfit(stockPrices) {
   var maxProfitAtTime = prices.map(function(currentPrice, index, prices) {
-    var maxPriceAfter = Math.max.apply(0, prices.slice(index));
-    return maxPriceAfter - currentPrice;
+    var maxPriceAfter = Math.max.apply(this, prices.slice(index));
+    var profit = maxPriceAfter - currentPrice;
+    return profit > 0 ? profit : -1;
   });
-  return Math.max.apply(0, maxProfitAtTime);
+  return Math.max.apply(this, maxProfitAtTime);
 }
 
-var prices = [45, 24, 35, 31, 40, 38, 11];
+// var prices = [45, 24, 35, 31, 40, 38, 11];
+var prices = [3,2,1];
 console.log(maxProfit(prices));
