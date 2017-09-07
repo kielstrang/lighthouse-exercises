ffbeb532682bf72b4cbdeb9b212509ce6d05396d
vagrant <kiel.strang@gmail.com>
2017-09-07 23:10:27 +0000

    Code review changes to stock-market.js

--- w1d5/stock-market/stock-market.js
+++ w1d5/stock-market/stock-market.js
@@ -1,10 +1,13 @@
+const max = arr => Math.max(...arr);
+const min = arr => Math.min(...arr);
+
 function maxProfit(stockPrices) {
   var maxProfitAtTime = stockPrices.map(function(currentPrice, index, prices) {
-    var maxPriceAfter = Math.max.apply(0, prices.slice(index));
+    var maxPriceAfter = max(prices.slice(index));
     var profit = maxPriceAfter - currentPrice;
     return profit > 0 ? profit : -1;
   });
-  return Math.max.apply(0, maxProfitAtTime);
+  return Math.max(...maxProfitAtTime);
 }
 
 function fastMaxProfit(stockPrices) {
@@ -13,11 +16,9 @@ function fastMaxProfit(stockPrices) {
   }
 
   var firstHalf = stockPrices.splice(0, Math.floor(stockPrices.length / 2));
-  var minInFirstHalf = Math.min.apply(0, firstHalf);
   var secondHalf = stockPrices;
-  var maxInSecondHalf = Math.max.apply(0, secondHalf);
 
-  return Math.max.apply(0, [fastMaxProfit(firstHalf), fastMaxProfit(secondHalf), maxInSecondHalf - minInFirstHalf]);
+  return max([max(secondHalf) - min(firstHalf), fastMaxProfit(firstHalf), fastMaxProfit(secondHalf)]);
 }
 
 var prices = [45, 24, 35, 31, 40, 38, 11];
