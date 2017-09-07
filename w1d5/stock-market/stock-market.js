87da48efd5e0836d26af9fc652b4f9deaeee2eeb
vagrant <kiel.strang@gmail.com>
2017-09-07 22:31:05 +0000

    Added recursive fastMaxProfit to stock-market.js

--- w1d5/stock-market/stock-market.js
+++ w1d5/stock-market/stock-market.js
@@ -1,12 +1,22 @@
 function maxProfit(stockPrices) {
   var maxProfitAtTime = prices.map(function(currentPrice, index, prices) {
-    var maxPriceAfter = Math.max.apply(this, prices.slice(index));
+    var maxPriceAfter = Math.max.apply(0, prices.slice(index));
     var profit = maxPriceAfter - currentPrice;
     return profit > 0 ? profit : -1;
   });
-  return Math.max.apply(this, maxProfitAtTime);
+  return Math.max.apply(0, maxProfitAtTime);
 }
 
-// var prices = [45, 24, 35, 31, 40, 38, 11];
-var prices = [3,2,1];
-console.log(maxProfit(prices));
+function fastMaxProfit(stockPrices) {
+  if(stockPrices.length <= 1) return -1;
+
+  var firstHalf = stockPrices.splice(0, Math.floor(stockPrices.length / 2));
+  var minInFirstHalf = Math.min.apply(0,firstHalf);
+  var secondHalf = stockPrices;
+  var maxInSecondHalf = Math.max.apply(0, secondHalf);
+
+  return Math.max.apply(0, [fastMaxProfit(firstHalf), fastMaxProfit(secondHalf), maxInSecondHalf - minInFirstHalf]);
+}
+
+var prices = [45, 24, 35, 31, 40, 38, 11];
+console.log(fastMaxProfit(prices));
