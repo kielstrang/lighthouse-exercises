3f8c6588880006e42659b7067df3e3662ed06381
vagrant <kiel.strang@gmail.com>
2017-09-07 18:30:32 +0000

    Add package exercise

---
+++ w1d5/npm-packages/index.js
@@ -0,0 +1,9 @@
+
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.blue("Earth");
+console.log(message);
+var anotherMessage = "Hello " + chalk.grey("Moon");
+console.log(anotherMessage);
+var yetAnotherMessage = "Hello " + chalk.red("Mars");
+console.log(yetAnotherMessage);
