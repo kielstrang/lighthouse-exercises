9332cefc014910429ff9c916ca4de7a5bba1a56b
vagrant <kiel.strang@gmail.com>
2017-09-07 21:19:43 +0000

    Add brute force version of stock-market.js

---
+++ w1d5/stock-market/stock-market.js
@@ -0,0 +1,11 @@
+function maxProfit(stockPrices) {
+  console.log(stockPrices);
+  var maxProfitAtTime = prices.map(function(currentPrice, index, prices) {
+    var maxPriceAfter = Math.max.apply(0, prices.slice(index));
+    return maxPriceAfter - currentPrice;
+  });
+  return Math.max.apply(0, maxProfitAtTime);
+}
+
+var prices = [45, 24, 35, 31, 40, 38, 11];
+console.log(maxProfit(prices));
